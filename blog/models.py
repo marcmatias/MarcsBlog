7eb1b8660cd83c1736222b611bec08b353ce88c5
marcmatias <marcmatias@me.com>
2017-05-15 11:57:44 -0300

    Blog funcional

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,13 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    #url = models.URLField(
+    #    blank=True, null=True)
     created_date = models.DateTimeField(
         default = timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,3 +21,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- mysite/settings.py
+++ mysite/settings.py
@@ -100,13 +100,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Brazil/East'
 
 USE_I18N = True
 
