5fd9fcb2734913462f443ca8e960db1aa53259a1
marcmatiasdev <marcmatiasdev@gmail.com>
2018-02-20 11:12:23 -0300

    removendo cached

--- blog/filters.py
+++
@@ -1,8 +0,0 @@
-from .models import Post
-import django_filters
-
-class TitleFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    class Meta:
-        model = Post
-        fields = ['title']
