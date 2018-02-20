406ff9870ab7b631a282c49e059de1f5a6b9e829
marcmatiasdev <marcmatiasdev@gmail.com>
2018-02-20 11:04:39 -0300

    corrigindo arquivos não trackeados

---
+++ blog/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Post, Comment
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .filters import TitleFilter
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def search(request):
+    post_list = Post.objects.all()
+    post_filter = TitleFilter(request.GET, queryset=post_list)
+    return render(request, 'blog/search.html', {'filter': post_filter})
+
+#def authors(request, pk):
+#    me = get_object_or_404(Post, pk=pk)
+#    posts = Post.objects.filter(author=me)
+    # user = Post.objects.get(Post, username=author)
+    # post = Post.objects.filter(author=user)
+#    return render(request, 'blog/authors.html', {'posts': posts})
+
+def authors(request):
+     me = User.objects.get(username=request.user)
+     posts = Post.objects.filter(author=me)
+    # user = Post.objects.get(Post, username=author)
+    # post = Post.objects.filter(author=user)
+     return render(request, 'blog/authors.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def about(request):
+    return render(request, 'blog/about.html', {})
